c5b91d882b6fac7961bcc7aebe397abd6f4bf10d
yonikesel1 <yonikesel1@gmail.com>
2022-03-27 18:35:39 +0300

    Added authors and added endpoint to return most liquid pair from sushiswap

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
-const express = require("express");
+import { createClient } from "@urql/core";
+import "isomorphic-unfetch";
+
+import express from "express";
 const app = express();
 
+const APIURL = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange";
+
+const query = `
+query {
+  pairs(orderBy: reserveUSD, orderDirection: desc){
+    id
+    name
+    reserveUSD
+    reserveETH
+    token0{
+      symbol
+    }
+    token1{
+      symbol
+    }
+    token0Price
+    token1Price
+  }
+}
+`;
+
+const client = createClient({
+  url: APIURL,
+});
+
 app.get("/", (req, res) => {
-  res.send("Creating Order out of Chaos");
+  res.send("Creating Order out of Chaos \n By Yoni and Laurence");
+});
+
+app.get("/Pairs", async (req, res) => {
+  const response = await client.query(query).toPromise();
+  console.log("response:", response);
+  res.send(response.data.pairs[1].name);
 });
 
 /* Port 8080 */
